2a615a91781cc469f4e453d24d80bd5d96422449
= <=>
2024-03-05 23:58:59 +0300

    fix2.0 some moment and create registration on path(/acc)

--- renderer/PageShell.tsx
+++ renderer/PageShell.tsx
@@ -22,16 +22,5 @@ function PageShell({
 }
 
 function Content({ children }: { children: React.ReactNode }) {
-	return (
-		<div
-		// style={{
-		// 	padding: 20,
-		// 	paddingBottom: 50,
-		// 	borderLeft: '2px solid #eee',
-		// 	minHeight: '100vh',
-		// }}
-		>
-			{children}
-		</div>
-	)
+	return <div>{children}</div>
 }

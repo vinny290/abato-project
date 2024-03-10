55d2ad60fa1d894e37326eb7e219fc4cd2e64f89
Daniel Ilich <d4niel.iv@gmail.com>
2024-03-10 18:35:05 +0300

    main(eslint): расширение конфига eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import react from '@vitejs/plugin-react'
-import vike from 'vike/plugin'
-import { UserConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import vike from "vike/plugin";
+import { UserConfig } from "vite";
 
 const config: UserConfig = {
-  plugins: [react(), vike()]
-}
+  plugins: [react(), vike()],
+};
 
-export default config
+// eslint-disable-next-line import/no-default-export
+export default config;
